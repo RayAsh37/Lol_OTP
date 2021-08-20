1e7afef3f90edab96b4e1e5dccc0b1cb1cab9dfd
RayAsh37 <ashrayshetty1999@gmail.com>
2021-08-20 15:34:20 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css'
+import axios from 'axios'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { Container } from '@material-ui/core'
+
+const App = () => {
+  const [champArray, setChampArray] = useState([])
+  const champsArrayGet = async () => {
+    try {
+      const data = await axios.get(
+        `http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion.json`
+      )
+      console.log(data.data.data)
+      setChampArray(data.data.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    champsArrayGet()
+  }, [])
+
+  return (
+    <>
+      <Container maxWidth='md'></Container>
+    </>
+  )
+}
+
+export default App
---
+++ src/Champ.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import './Champ.css'
+
+const Champ = () => {
+  const [champ, setChamp] = useState([])
+
+  return (
+    <>
+      <div>
+        <h2>champName</h2>
+        <img src='' alt='' />
+      </div>
+    </>
+  )
+}
+
+export default Champ
