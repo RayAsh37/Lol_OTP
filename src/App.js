80c0614f6a5fa149ee691f87c149e9e63d446bd7
RayAsh37 <ashrayshetty1999@gmail.com>
2021-08-20 15:51:43 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import './App.css'
-import axios from 'axios'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { Container } from '@material-ui/core'
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Container } from '@material-ui/core';
 
 const App = () => {
-  const [champArray, setChampArray] = useState([])
+  const [champArray, setChampArray] = useState([]);
   const champsArrayGet = async () => {
     try {
       const data = await axios.get(
         `http://ddragon.leagueoflegends.com/cdn/11.16.1/data/en_US/champion.json`
-      )
-      console.log(data.data.data)
-      setChampArray(data.data.data)
+      );
+      console.log(data.data.data);
+      setChampArray(data.data.data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   useEffect(() => {
-    champsArrayGet()
-  }, [])
+    champsArrayGet();
+  }, []);
 
   return (
     <>
-      <Container maxWidth='md'></Container>
+      <Container maxWidth="md">Hello</Container>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
